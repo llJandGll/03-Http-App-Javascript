45460c6dada0c5d148c27c565f5e8fb3c3fa6f59
Jesus Zeballos <IIJandGII@gmail.com>
2024-11-13 12:32:29 -0400

    comentaries delete

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -9,7 +9,6 @@ import { localhostUserToModel } from "../mappers/localhost-user.mapper";
 
 export const loadUsersByPage = async ( page = 1 ) => {
   const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;
-  console.log(url);
   
   const connection = await fetch( url );
   const datos = await connection.json();
